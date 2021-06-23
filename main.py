120519d2b2956d4a309e7caab15d56f2ce46d653
franhermani <franhermani@gmail.com>
2021-06-23 00:52:35 -0300

    Main skeleton and csv files

---
+++ main.py
@@ -0,0 +1,19 @@
+def main():
+    distances_path = "distancias.csv"
+    derbies_path = "clasicos.csv"
+    distances = {}
+    derbies = {}
+    read_distances(distances_path, distances)
+    read_derbies(derbies_path, derbies)
+    print(distances)
+
+
+def read_distances(path, distances):
+    return 0
+
+
+def read_derbies(path, derbies):
+    return 0
+
+
+main()
