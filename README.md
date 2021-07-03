de14e22859a36af3deea6c2286c9b42a0176c5cf
franhermani <franhermani@gmail.com>
2021-07-03 20:13:09 -0300

    Calculate variance with hardcoded fixture

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# fixture-para-todos
-Fixture Para Todos - TP Modelos y Optimización - FIUBA
+# Fixture Para Todos - TP Modelos y Optimización - FIUBA
+
+## Problema a resolver
+Armado del fixture de una liga de fútbol minimizando la varianza de los kilómetros de viaje de todos los equipos
+
+## Consideraciones
+- Todos los equipos se enfrentan entre sí una única vez
+- El sistema define únicamente los enfrentamientos
+
+## Restricciones
+- Todos los equipos deben jugar la misma cantidad de partidos de local y de visitante que el resto
+- Todos los equipos deben jugar la misma cantidad de partidos de local y de visitante o, a lo sumo, un partido de diferencia entre ambas condiciones
+- Ningún equipo puede jugar más de tres veces dos partidos consecutivos en la misma condición
+- Los equipos considerados clásicos entre sí no pueden jugar en la misma condición en la misma fecha
+
+## Relación con Investigación Operativa
+- Problema complejo de resolver
+- Solución óptima tomaría mucho tiempo
+- Solución aproximada con herramientas de IO
+
+## Beneficios Esperados
+- Fixture equitativo para todos los equipos
+- Equipos con más peso dejan de inclinar la balanza a su favor
+- Solución más justa para todos
+
+## Heurística
+### Paso 1
+Armar una solución inicial factible tomando siempre en cada fecha el rival más cercano, siempre considerando
+que los dos equipos no se hayan enfrentado aún y que se cumplan las restricciones mencionadas.
+Una vez completa la solución, calcular la varianza de los kilómetros recorridos por todos los equipos.
+
+### Paso 2
+
+### Paso 3
+
+### Paso 4
--- main.py
+++ main.py
@@ -1,19 +1,142 @@
+import csv
+import math
+
+
 def main():
-    distances_path = "distancias.csv"
-    derbies_path = "clasicos.csv"
+    distances_path, derbies_path = "csv/distancias_test.csv", "csv/clasicos_test.csv"
+
+    distances = load_distances(distances_path)
+    derbies = load_derbies(derbies_path)
+
+    fixture = calculate_fixture(distances, derbies)
+    print(calculate_km_traveled_variance(fixture, distances))
+
+
+# Carga las distancias a partir del archivo csv
+def load_distances(path):
+    '''
+        Formato de las distancias:
+        {"Equipo A": {"Equipo B": 100, ..., "Equipo Z": 200},
+        ...,
+        "Equipo X": {"Equipo Y": 150, ..., "Equipo Z": 300}
+        }
+    '''
+
     distances = {}
+    header = True
+
+    with open(path) as csv_file:
+        csv_distances = csv.reader(csv_file, delimiter=",")
+        for row in csv_distances:
+            if header:
+                header = False
+                continue
+
+            team1, team2, distance = row[0], row[1], row[2]
+
+            if team1 not in distances:
+                distances[team1] = {}
+            distances[team1][team2] = distance
+
+            if team2 not in distances:
+                distances[team2] = {}
+            distances[team2][team1] = distance
+
+    return distances
+
+
+# Carga los derbies a partir del archivo csv
+def load_derbies(path):
+    '''
+    Formato de los derbies:
+        {"Equipo A": "Equipo B",
+        ...,
+        "Equipo Y": "Equipo Z"
+        }
+    '''
+
     derbies = {}
-    read_distances(distances_path, distances)
-    read_derbies(derbies_path, derbies)
-    print(distances)
+    header = True
+
+    with open(path) as csv_file:
+        csv_derbies = csv.reader(csv_file, delimiter=",")
+        for row in csv_derbies:
+            if header:
+                header = False
+                continue
+
+            team1, team2 = row[0], row[1]
+
+            if team1 not in derbies:
+                derbies[team1] = team2
+
+            if team2 not in derbies:
+                derbies[team2] = team1
+
+    return derbies
 
 
-def read_distances(path, distances):
-    return 0
+# Calcula el fixture del torneo minimizando la varianza
+# de los km de viaje de todos los equipos
+def calculate_fixture(distances, derbies):
+    '''
+    Formato del fixture:
+        {"Fecha 1":
+            [("Equipo A", "Equipo B"),
+            (..., ...),
+            ("Equipo Y", "Equipo Z")],
+        ...,
+        "Fecha N":
+            [("Equipo A", "Equipo B"),
+            (..., ...),
+            ("Equipo Y", "Equipo Z")]
+        }
+    '''
+    fixture = {
+        1: [("Boca Juniors", "River Plate"), ("Belgrano", "Talleres")],
+        2: [("Talleres", "Boca Juniors"), ("River Plate", "Belgrano")],
+        3: [("Boca Juniors", "Belgrano"), ("River Plate", "Talleres")],
+    }
+    return fixture
 
 
-def read_derbies(path, derbies):
-    return 0
+def calculate_km_traveled_variance(fixture, distances):
+    # Calcula la varianza actual de los km de viaje de todos los equipos
+    km_traveled_variance = 0
+    km_traveled_per_team = calculate_km_traveled_per_team(fixture, distances)
+    media = calculate_km_traveled_media(km_traveled_per_team)
+
+    for distance in km_traveled_per_team.values():
+        km_traveled_variance += (distance - media)**2
+    km_traveled_variance/len(km_traveled_per_team)
+
+    return math.sqrt(km_traveled_variance)
+
+
+# Calcula los km recorridos por cada equipo
+def calculate_km_traveled_per_team(fixture, distances):
+    '''
+    Formato de los km recorridos por equipo:
+        {"Equipo A": 100,
+        ...,
+        "Equipo Z": 200
+        }
+    '''
+    km_traveled_per_team = {}
+    for matches in fixture.values():
+        for match in matches:
+            local_team, away_team = match[0], match[1]
+
+            if away_team not in km_traveled_per_team:
+                km_traveled_per_team[away_team] = 0
+            km_traveled_per_team[away_team] += int(distances[away_team][local_team])
+
+    return km_traveled_per_team
+
+
+# Calcula la media de los km recorridos por los equipos
+def calculate_km_traveled_media(km_traveled_per_team):
+    return sum(km_traveled_per_team.values())/len(km_traveled_per_team)
 
 
 main()
